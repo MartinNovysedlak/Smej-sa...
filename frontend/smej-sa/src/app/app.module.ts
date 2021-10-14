c7734ddc8e43eea8269efd39a35cc848521ef268
ŠimonČanecký <simoncanecky1@gmail.com>
2021-10-14 11:08:45 +0200

    design

--- frontend/smej-sa/src/app/app.module.ts
+++ frontend/smej-sa/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card"
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {MatIconModule} from "@angular/material/icon";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
