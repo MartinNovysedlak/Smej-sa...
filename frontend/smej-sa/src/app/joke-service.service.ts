d8243e372308040b7a37ae5211a9b8d7d0cfd160
BayFirer <xkultan@fi.muni.cz>
2021-11-01 10:05:41 +0100

    Added jokeservice

---
+++ frontend/smej-sa/src/app/joke-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Vtipy } from './vtipy';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokeServiceService {
+ vtipyUrl="http://localhost:8081/Vtip"
+  constructor(private http: HttpClient) { }
+
+  getJokes():Observable<Vtipy>{
+    return this.http.get<Vtipy>(this.vtipyUrl)
+}
+}
