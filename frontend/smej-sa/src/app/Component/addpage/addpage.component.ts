f88db36a74452ab5fe3a9ac6f7f7d437d25ef111
BayFirer <xkultan@fi.muni.cz>
2022-02-02 21:34:37 +0100

    Working on adding jokes

--- frontend/smej-sa/src/app/Component/addpage/addpage.component.ts
+++ frontend/smej-sa/src/app/Component/addpage/addpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addpage',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddpageComponent implements OnInit {
 
+  userJokeForm = new FormGroup({
+    joke: new FormControl("", Validators.required),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  addJoke(){
+    console.log(this.userJokeForm.value)
   }
 
 }
